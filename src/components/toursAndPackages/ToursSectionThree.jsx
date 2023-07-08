f7b1e619f2c0baf2588f77b5332cf041a4cd5529
Anurag Mourya <anuragengee@gmail.com>
2023-07-08 16:37:05 +0530

    created toure section

---
+++ src/components/toursAndPackages/ToursSectionThree.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ToursSectionThree = () => {
+    return (
+        <div>ToursSectionThree</div>
+    )
+}
+
+export default ToursSectionThree
--- src/pages/ToursAndPackages.jsx
+++ src/pages/ToursAndPackages.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ToursSectionOne from '../components/toursAndPackages/ToursSectionOne'
 import ToursSectionTwo from '../components/toursAndPackages/ToursSectionTwo'
+import { Container } from 'react-bootstrap'
+import ToursSectionThree from '../components/toursAndPackages/ToursSectionThree'
 
 
 
 const ToursAndPackages = () => {
     return (
         <div>
+
             <ToursSectionOne />
-            <ToursSectionTwo />
+            <Container>
+                <ToursSectionTwo />
+                <ToursSectionThree />
+            </Container>
+
         </div>
     )
 }
